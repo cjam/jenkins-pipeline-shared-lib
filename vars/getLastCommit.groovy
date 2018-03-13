3a0176d409fe71f1cd913778ef002ec752fa90f0
Colter McQuay <colter.mcquay@roughdraftdev.com>
2018-03-13 06:39:32 -0700

    getLastCommit Fix: - Instead of grabbing the most recent, it was grabbing the first

--- vars/getLastCommit.groovy
+++ vars/getLastCommit.groovy
@@ -2,7 +2,7 @@ def String call() {
   def commit = null
   def changeLogSets = currentBuild.changeSets
   if(changeLogSets.size()>0 && changeLogSets.items.size() > 0){
-    commit = changeLogSets[0].items[0]
+    commit = changeLogSets[0].items[changeLogSets.items.size()-1]
   }
   return commit
 }
